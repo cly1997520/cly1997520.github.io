b8dc95065456c2349603ec7c6ba47ffd116283c7
cly1997520 <1045677816@qq.com>
2017-12-07 15:13:52 +0800

    footer1

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -26,3 +26,153 @@ var swiper = new Swiper('.swiper-container', {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//*{
+//    margin: 0;
+//    padding: 0;
+//    box-sizing: border-box;
+//}
+//html, body {
+//    position: relative;
+//    height: 100%;
+//}
+//body {
+//    background: #eee;
+//    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+//    font-size: 14px;
+//    color:#000;
+//    margin: 0;
+//    padding: 0;
+//}
+//.swiper-container {
+//    width: 100%;
+//    height: 100%;
+//}
+//.swiper-slide {
+//    text-align: center;
+//    font-size: 18px;
+//    background: #fff;
+//}
+//.swiper-button-next{
+//    margin-left: -13.5px;
+//    left:50%;
+//    right:auto;
+//    top:auto;
+//    bottom:5%;
+//    transform: rotate(90deg);
+//}
+//
+//
+///**.s1第一屏样式*/
+//.s1{
+//    background: url("../img/bj.png");
+//    height: 100%;
+//    position: relative;
+//}
+//.s1 .header img:nth-of-type(1){
+//    width: 30px;
+//    position: absolute;
+//    top: 127px;
+//    left: 49px;
+//}
+//.s1 .header img:nth-of-type(2){
+//    width: 53px;
+//    position: absolute;
+//    top: 81px;
+//    right: 37px;
+//}
+//.s1 .header img:nth-of-type(3){
+//    width: 53px;
+//    position: absolute;
+//    top:405px;
+//    left: 18px;
+//
+//}
+//.s1 .header img:nth-of-type(4){
+//    width: 40px;
+//    position: absolute;
+//    top:404px;
+//    right: 56px;
+//}
+//.s1 .header img:nth-of-type(5){
+//    width: 66%;
+//    position: absolute;
+//    top:175px;
+//    left: 16%;
+//}
+//.s1 .header img:nth-of-type(6){
+//    width: 2%;
+//    position: absolute;
+//    top:22px;
+//    left: 346px;
+//    transform: rotate(60deg);
+//
+//}
+//
+///*第一屏文字样式*/
+//.s1 .main{
+//    position: relative;
+//}
+//.s1 .main img:nth-of-type(1){
+//
+//    width: 140px;
+//    position: absolute;
+//    top:211px;
+//    left: 30%;
+//}
+//.s1 .main img:nth-of-type(2){
+//    width: 81%;
+//    position: relative;
+//    top:265px;
+//
+//}
+//.s1 .main .img9{
+//    background: url("../img/9.png");
+//    background-size:cover;
+//    width: 207px;
+//    position: absolute;
+//    top: 354px;
+//    left:22%;
+//    line-height: 23px;
+//    font-weight: bold;
+//
+//}
+///*footer部分*/
+//.s1 .footer{
+//    position: absolute;
+//    bottom: 10%;
+//    width: 100%;
+//}
+//.s1 .footer p{
+//    color:#0ca996;
+//    font-size:16px;
+//    font-weight:bold;
+//    line-height: 2;
+//}
+//.s1 .footer img:nth-of-type(1){
+//    position: absolute;
+//    top: 139px;
+//    height: 70px;
+//    left: -102px;
+//
+//}
+//.s1 .footer img:nth-of-type(2){
+//    position: relative;
+//    top: 90px;
+//    height: 70px;
+//}
